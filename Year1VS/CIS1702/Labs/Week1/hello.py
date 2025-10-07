33f40afcd010c5881f34efc3b531ad8492f8bbfc
RosieWitkowski <rosiewitkowski@gmail.com>
2025-10-07 15:22:30 +0100

    First commit

---
+++ Year1VS/CIS1702/Labs/Week1/hello.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+name = "Rosie Witkowksi"
+height = 1.56 
+
+print("Name:", name)
+print(f"Height: {height}m")
+
+def add(a, b):
+    print(f"{a} + {b} = ", end="")
+    return a + b
+
+print(add(4, 3)) 
+print(add(12, 32))
