3d725f64a9fe76ff54d97753f09520a8d8d5bd3f
RosieWitkowski <rosiewitkowski@gmail.com>
2025-11-04 22:28:18 +0000

    Ascii art sandbox.

---
+++ Year1VS/CIS1702/practice/ascii_art.py
@@ -0,0 +1,31 @@
+# Remember to use r for raw text and triple quotation for escaping 
+def happy_wizard():
+    print(r"""
+          *
+        /   \
+        ^ | ^
+          V     *    
+        #####  /
+    """)
+
+def unhappy_wizard():
+    print(r"""
+          *
+        /   \
+        ಠ | ಠ
+          ^    *    
+        #####  |
+    """)
+
+def shocked_wizard():
+    print(r"""
+            *
+          /   \
+          O | O
+            O     *    
+        \ #####  /
+    """)
+
+happy_wizard()
+unhappy_wizard()
+shocked_wizard()
