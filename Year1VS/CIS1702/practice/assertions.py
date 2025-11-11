2c13972b4188b49dbc5216235fd0a01a36f9eb46
RosieWitkowski <rosiewitkowski@gmail.com>
2025-11-11 19:32:17 +0000

    Added unit test 2 and 3.

--- Year1VS/CIS1702/practice/assertions.py
+++ Year1VS/CIS1702/practice/assertions.py
@@ -1,6 +1,7 @@
 def add(a, b):
     return a + b 
 
+'''
 # -- Unit Test -- 
 print("Running tests... ")
 assert add(2, 4) == 6
@@ -11,3 +12,15 @@ print("Testing error messages... ")
 assert add(5, 8) == 50, 'Expected output was 13'
 assert add(9, 100) == False, 'Expected output was an int, not bool' # Note won't reach because it returns on line 11
 print("Unexpected")
+
+'''
+
+# -- Unit Test 2-- 
+try:
+    assert add(5, 2) == 100
+except AssertionError:
+    print("5+2 != 100")
+
+
+# -- Unit Test 3 -- 
+assert add(5, 2) == 100, '5+2 != 100' # Note, WILL reach because the earlier test catches the example rather than returning
