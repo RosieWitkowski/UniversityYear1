33b40bf4de11d3940fdad7a786203e61d2c5ae82
RosieWitkowski <rosiewitkowski@gmail.com>
2025-11-11 21:40:49 +0000

    Dict exercises.

--- Year1VS/CIS1702/Labs/week6/dict_phonebook.py
+++ Year1VS/CIS1702/Labs/week6/dict_phonebook.py
@@ -2,3 +2,18 @@ phonebook = {}
 
 phonebook["Alice"] = "555-1234"
 phonebook["Bob"] = "555-5678"
+
+# Searching
+name_search = input("Name: ").capitalize()
+if name_search in phonebook:
+    print(f"Number: {phonebook[name_search]}")
+else:
+    print("Name not found.")
+
+# Adding 
+name, number = input("Name: ").capitalize(), input("Number: ")
+phonebook[name] = number
+
+# Printing
+for name, number in phonebook.items():
+    print(f"{name}: {number}")
