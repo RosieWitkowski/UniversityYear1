eeecd2da98e06d8bdf6105ee81600dc0668fe410
RosieWitkowski <rosiewitkowski@gmail.com>
2025-11-13 22:14:58 +0000

    Made damage a seperate var

--- Year1VS/CIS1702/Labs/week6/scope_fix.py
+++ Year1VS/CIS1702/Labs/week6/scope_fix.py
@@ -1,5 +1,5 @@
-# --- Bugged code ---
 player_health = 100 
+# --- Bugged code ---
 def take_damage():
     player_health = 90
     print("Player took damage!")
@@ -8,10 +8,11 @@ take_damage()
 print(f"Player health is {player_health}")
 
 # --- Fixed code ---
+DAMAGE = 20
+
 def take_damage(player_health):
-    player_health = 90
     print("Player took damage!")
-    return player_health
+    return player_health - DAMAGE
 
 player_health = 100     
 player_health = take_damage(player_health)
