2d3160bafe5c76c194346960e99b7879e302fc52
RosieWitkowski <rosiewitkowski@gmail.com>
2025-11-13 19:29:01 +0000

    Added exit method and sleep intervals

--- Year1VS/CIS1702/practice/wizard_class.py
+++ Year1VS/CIS1702/practice/wizard_class.py
@@ -1,10 +1,19 @@
+from time import sleep 
+
 class wizard():
     def _init_(self):
         pass 
 
-    def enter():
+    def enter(interval):
         print('An odd approaching shape reveals itself to be...')
+        sleep(interval)
         print("Nyahaheheh, it's me! Can you answer my riddles three?")
+        sleep(interval)
+    
+    def exit(interval):
+        print("Enough of this nonsense!")
+        sleep(interval)
+        print("*The wizard has left*")
 
     def happy():
         print(r"""
@@ -26,7 +35,20 @@ class wizard():
                *
         """)
     
+EMOTE_INTERVAL = 1.25
+
 odd_wizard = wizard 
-wizard.enter()
+wizard.enter(1.5)
 wizard.happy()
+sleep(EMOTE_INTERVAL)
 wizard.sad()
+sleep(EMOTE_INTERVAL)
+wizard.exit(1)
+
+sleep(2)
+
+wizard.enter(1)
+print("Ahaha, I'll give you another try!")
+sleep(1)
+wizard.happy()
+sleep(EMOTE_INTERVAL // 2)
