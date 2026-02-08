5d399b1379af91def41f0eff8953a5b0242d314d
RosieWitkowski <rosiewitkowski@gmail.com>
2026-02-08 17:20:05 +0000

    Change to strings

--- Year1VS/study_motivator/file_manager.py
+++ Year1VS/study_motivator/file_manager.py
@@ -3,11 +3,13 @@ import json
 def save_file(data, filename: str) -> None:
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
+        print("> File saved sucessfully")
 
 
 def load_file(filename: str) -> list[dict]:
     try:
         with open(filename, 'r') as f:
+            print("> File loaded successfully")
             return json.load(f)
     except FileNotFoundError:
         print("[!] Error: File could not be found.")
