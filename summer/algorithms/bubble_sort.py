ea19cf86a7ba5510500c26542ad092b96cb4c4bf
Rose Pose <rosiewitkowski@gmail.com>
2026-09-12 21:17:25 +0100

    Revision of sorting algorithms - bubble sort

---
+++ summer/algorithms/bubble_sort.py
@@ -0,0 +1,15 @@
+list_example = [0, 5, 8, 2, 3, 7]
+counter, swaps, length = 0, 1 , len(list_example)
+
+while swaps > 0:
+    for i in range(0, length-1):
+        if list_example[i] > list_example[i+1]:
+            temp = list_example[i]
+            list_example[i] = list_example[i+1]
+            list_example[i+1] = temp 
+            swaps += 1
+
+print(list_example)
+
+
+    
