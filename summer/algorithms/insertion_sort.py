9d8b4b1033f9c223e404f9abc9ebd0a6c4802a11
Rose Pose <rosiewitkowski@gmail.com>
2026-09-13 22:15:28 +0100

    Optomised

--- summer/algorithms/insertion_sort.py
+++ summer/algorithms/insertion_sort.py
@@ -3,9 +3,11 @@ def insertion_sort(list_example):
     length = len(copy_list)
     for i in range(1, length):
         insert_index = i 
-        current_val = copy_list.pop(i)
+        current_val = copy_list.pop(i) 
         for j in range(i-1, -1, -1):
             if copy_list[j] > current_val:
                 insert_index = j 
+            else:
+                break
         copy_list.insert(insert_index, current_val)
     return copy_list
