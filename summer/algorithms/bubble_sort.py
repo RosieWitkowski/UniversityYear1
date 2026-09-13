c7a692c589c29704dbaab5b615687bc45e622c37
Rose Pose <rosiewitkowski@gmail.com>
2026-09-13 01:00:00 +0100

    Print statements

--- summer/algorithms/bubble_sort.py
+++ summer/algorithms/bubble_sort.py
@@ -1,7 +1,10 @@
 list_example = [0, 5, 8, 2, 3, 7]
 counter, swaps, length = 0, 1 , len(list_example)
 
+print(f"Unsorted: {list_example}")
+
 while swaps > 0:
+    counter, swaps = 0, 0 
     for i in range(0, length-1):
         if list_example[i] > list_example[i+1]:
             temp = list_example[i]
@@ -9,7 +12,7 @@ while swaps > 0:
             list_example[i+1] = temp 
             swaps += 1
 
-print(list_example)
+print(f"Sorted: {list_example}")
 
 
     
